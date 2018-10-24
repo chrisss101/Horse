dade39d898b408750ccb578a952061d24e5a533f
BT_1N3_03@bktech.local <BT_1N3_03@bktech.local>
2018-10-24 12:25:39 -0400

    Initial commwdit

---
+++ src/Horse.java
@@ -0,0 +1,14 @@
+public class theHorse  {
+    private String name;
+    private int weight;
+    public void Horse(name,weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getWeight() {
+        return this.weight;
+    }
+}
---
+++ src/horseBarnRunner.java
@@ -0,0 +1,10 @@
+public class horseBarnRunner {
+    public static void main (String[] args) {
+        Horse horse1 = new Mustang(century,4000);
+        Horse horse2 = new Mustang(flame,6000);
+        Horse horse3 = new Mustang(aura,5000);
+        Horse[] horses = new Horse[6];
+        horses[1] = horse1;
+        horses[2] = horse2;
+    }
+}
