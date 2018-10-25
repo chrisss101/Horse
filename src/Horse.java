d51421c8741a623b85a2802fa9f28dc6d974b0ca
chrisss101 <Crafaniello9535@bths.edu>
2018-10-25 09:28:13 -0400

    wer

--- src/Horse.java
+++ src/Horse.java
@@ -1,14 +1,2 @@
-public class theHorse  {
-    private String name;
-    private int weight;
-    public void Horse(name,weight) {
-        this.name = name;
-        this.weight = weight;
-    }
-    public String getName() {
-        return this.name;
-    }
-    public int getWeight() {
-        return this.weight;
-    }
+public interface Horse {
 }
---
+++ src/HorseBarn.java
@@ -0,0 +1,11 @@
+public class HorseBarn {
+    public int findHorseSpace(String name) {
+        for (int i =0; i < this.spaces.length; i++) {
+            if((this.spaces[i] != null) && (name.equals(this.spaces[i].getName()))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+}
