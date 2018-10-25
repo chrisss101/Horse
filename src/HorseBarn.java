587620b94f499eaca31fbebbca921dc3b08dda76
chrisss101 <Crafaniello9535@bths.edu>
2018-10-25 09:41:14 -0400

    wer

--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -7,5 +7,17 @@ public class HorseBarn {
         }
         return -1;
     }
-    
+    public void consolidate() {
+        for (int i = 0;i < this.spaces.length-1; i++ ) {
+            if(this.spaces[i] == null) {
+                for (int j = i+1; i < this.spaces.length; j++) {
+                    if (this.spaces[j] != null) {
+                        this.spaces[i] = this.spaces[j];
+                        this.spaces[j] = null;
+                        j = this.spaces.length;
+                    }
+                }
+            }
+        }
+    }
 }
